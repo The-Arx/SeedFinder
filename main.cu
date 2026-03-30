9ad902b2223be94855870a5a854d19e755084583
The-Arx <115192551+The-Arx@users.noreply.github.com>
2026-03-30 00:19:28 -0700

    add negative bluprint brainstorm finder to cuda

--- main.cu
+++ main.cu
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cuda_runtime.h>
 
+#include "items.h"
 #include "seed.cu"
 
 __device__ bool find_negatives(const Seed &seed) {
@@ -12,6 +13,19 @@ __device__ bool find_negatives(const Seed &seed) {
     return true;
 }
 
+__device__ bool negative_bluestorm(const Seed &seed) {
+    RandGen shop_edition = seed.init_rand(7, "edisho1");
+    if (shop_edition.random() <= 0.997) return false;
+    if (shop_edition.random() <= 0.997) return false;
+    RandGen shop_rarity = seed.init_rand(10, "rarity1sho");
+    if (shop_rarity.random() <= 0.95) return false;
+    if (shop_rarity.random() <= 0.95) return false;
+    RandGen shop_rare = seed.init_rand(10, "Joker3sho1");
+    if (shop_rare.rand_item<Rare>() != Rare::Blueprint) return false;
+    if (shop_rare.rand_item<Rare>() != Rare::Brainstorm) return false;
+    return true;
+}
+
 __global__ void search_seeds() {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     int totalThreads = gridDim.x * blockDim.x;
@@ -20,7 +34,7 @@ __global__ void search_seeds() {
     long end_seed = total * (tid + 1) / totalThreads;
     Seed seed(start_seed);
     for (long i = start_seed; i < end_seed; i++) {
-        if (find_negatives(seed)) {
+        if (negative_bluestorm(seed)) {
             printf("%s\n", seed.seed);
         }
         seed.next();
